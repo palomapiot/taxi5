02258074d6c3568aed9b0977c650f4d119d1bd76
Fabián Lema Pérez <fabian.lema@udc.es>
2019-03-25 17:15:01 +0100

    Diseño Login App Taxista
    Se implementa el diseño del Login de la aplicación del taxista.

--- taxi5-taxista-android/app/src/main/java/com/muei/apm/taxi5driver/MainActivity.java
+++ taxi5-taxista-android/app/src/main/java/com/muei/apm/taxi5driver/MainActivity.java
@@ -1,13 +1,34 @@
 package com.muei.apm.taxi5driver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+        }
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnLogin(View view) {
+        btnLogin = findViewById(R.id.btnLogin);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickBtnRegister(View view) {
+        /*btnRegister = (Button) findViewById(R.id.btnRegister);
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);*/
     }
 }
