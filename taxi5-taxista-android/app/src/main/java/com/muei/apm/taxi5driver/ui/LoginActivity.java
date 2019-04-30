910c1b683ff0c123bfad7d594d4c1a6dd6912138
belen <belen@10.20.38.85>
2019-04-30 13:50:29 +0200

    eula taxista dialogo login

--- taxi5-taxista-android/app/src/main/java/com/muei/apm/taxi5driver/ui/LoginActivity.java
+++ taxi5-taxista-android/app/src/main/java/com/muei/apm/taxi5driver/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.pm.PackageManager;
@@ -15,6 +16,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -324,8 +326,28 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent(LoginActivity.this, TravelsActiveActivity.class);
-                startActivity(intent);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(LoginActivity.this);
+                // Configura el titulo.
+                alertDialogBuilder.setTitle(R.string.eula_dialog_title);
+                // Configura el mensaje.
+                alertDialogBuilder
+                        .setMessage(R.string.eula_dialog)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // TODO guardar login
+                                Intent intent = new Intent(LoginActivity.this, TravelsActiveActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).create().show();
+
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
