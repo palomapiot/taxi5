28079435b2ab42d47986e41ce83dc9c859ee840b
Fabián Lema Pérez <fabian.lema@udc.es>
2019-05-07 20:32:13 +0200

    Correciones
    A mayores se corrige el mismo error de que no se muestra el nombre con el login normal

--- taxi5-android/app/src/main/java/com/muei/apm/taxi5/ui/ProfileActivity.java
+++ taxi5-android/app/src/main/java/com/muei/apm/taxi5/ui/ProfileActivity.java
@@ -6,16 +6,27 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.muei.apm.taxi5.R;
+import com.muei.apm.taxi5.api.APIService;
+import com.muei.apm.taxi5.api.ApiObject;
+import com.muei.apm.taxi5.api.ApiUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button buttonPass;
+    private APIService mAPIService;
+    private Long currentUserId = null;
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     private TextView tvName;
 
@@ -40,12 +51,31 @@ public class ProfileActivity extends AppCompatActivity {
 
         if (sharedPreferences.getBoolean("LOGINGOOGLE", loginGoogle)) {
             buttonPass.setVisibility(View.INVISIBLE);
+            tvName = findViewById(R.id.tvName);
+            tvName.setText(sharedPreferences.getString("NAME", ""));
         } else {
             buttonPass.setVisibility(View.VISIBLE);
+            mAPIService = ApiUtils.getAPIService();
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+            currentUserId = prefs.getLong("currentUserId", 0);
+            // TODO: get user id
+            mAPIService.getUserDetails(currentUserId).enqueue(new Callback<ApiObject>() {
+                @Override
+                public void onResponse(Call<ApiObject> call, Response<ApiObject> response) {
+                    if (response.isSuccessful()) {
+                        Log.i("API", "current user get submitted to API." + response.body().toString());
+                        currentUserId = response.body().id;
+                        tvName.setText(response.body().firstName + " " + response.body().lastName);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ApiObject> call, Throwable t) {
+                    Log.i("API", "Unable to get current user from API.");
+                }
+            });
         }
 
-        tvName = findViewById(R.id.tvName);
-        tvName.setText(sharedPreferences.getString("NAME", ""));
 
     }
 
@@ -60,7 +90,7 @@ public class ProfileActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
         startActivityForResult(intent, 0);
         return true;
