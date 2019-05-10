b774c96a2cc403d18ae4af79f9d4686375ae7cf7
Fabián Lema Pérez <fabian.lema@udc.es>
2019-05-10 17:51:49 +0200

    Correción de la ubicación actual

--- taxi5-android/app/src/main/java/com/muei/apm/taxi5/ui/HomeActivity.java
+++ taxi5-android/app/src/main/java/com/muei/apm/taxi5/ui/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -41,6 +42,9 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     LocationManager mLocationManager;
     Marker marker;
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
 
     private FirebaseAuth mAuth;
 
@@ -108,6 +112,28 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Toast.makeText(HomeActivity.this, getString(R.string.no_current_location), Toast.LENGTH_SHORT).show();
             }
         }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
     }
 
     private void configureMaps() {
